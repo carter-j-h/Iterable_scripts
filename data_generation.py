47ae6619eb4a596ded22a6c079bab460c30801cf
Carter Hickingbotham <carterhickingbotham@gmail.com>
2017-12-11 16:52:33 -0800

    found correct function to create flexible event distribution

--- data_generation.py
+++ data_generation.py
@@ -1,3 +1,5 @@
+import numpy
+
 from faker import Faker
 from iterablepythonwrapper.client import IterableApi
 
@@ -6,18 +8,18 @@ APIKEY="94c3333a8e224b32b93a40788d1927cc"
 
 
 
-
 class DataGeneration(object):
 	"""docstring for DataGeneration"""
-	def __init__(self, users=0, events=0):
+	def __init__(self, users=0, events=0, api_key):
 		
 		self.users = users
 		self.events= events
+		self.api_key= api_key
 
 
 	def create_users(self):
 		fake=Faker()
-		ic = IterableApi(api_key=APIKEY)
+		ic = IterableApi(api_key=self.api_key)
 
 		for i in range(self.users):
 
@@ -28,6 +30,14 @@ class DataGeneration(object):
 			print(u)
 
 			ic.update_user(email=email, data_fields=u)
+
+
+	def create_events(self):
+
+		# creates distibution of events 
+		events = numpy.random.randint(low=5, high=20, size=self.events)
+
+
 
 
 
